05664800000e39108599953ac57b18e750aac93e
Fr0stbyteR <renshihong@hotmail.com>
2021-11-16 01:47:03 +0800

    Update to libfaust 2.38.4. Set version to 0.6.53.

--- src/LibFaustLoader.js
+++ src/LibFaustLoader.js
@@ -7,21 +7,6 @@ class LibFaustLoader {
             "libfaust-wasm.data": dataLocation
         }[path]) || dir + path;
         const libFaust = await LibFaust({ locateFile });
-        libFaust.lengthBytesUTF8 = (str) => {
-            let len = 0;
-            for (let i = 0; i < str.length; ++i) {
-                let u = str.charCodeAt(i);
-                // eslint-disable-next-line no-mixed-operators
-                if (u >= 55296 && u <= 57343) u = 65536 + ((u & 1023) << 10) | str.charCodeAt(++i) & 1023;
-                if (u <= 127) ++len;
-                else if (u <= 2047) len += 2;
-                else if (u <= 65535) len += 3;
-                else if (u <= 2097151) len += 4;
-                else if (u <= 67108863) len += 5;
-                else len += 6;
-            }
-            return len;
-        };
         return libFaust;
     }
 }
